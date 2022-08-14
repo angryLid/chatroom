181a0cf381b1afc05d447eeaa24f3e8420a7c1b7
angrylid <zm4399@gmail.com>
2022-08-14 20:44:24 +0800

    update url

--- lib/service/conf.ts
+++ lib/service/conf.ts
@@ -2,7 +2,7 @@ import { FirebaseOptions, initializeApp } from "firebase/app";
 
 const baseURL =
   process.env.NODE_ENV === "production"
-    ? "https://chatroom-two.vercel.app"
+    ? location.origin
     : "http://localhost:5669";
 const getConfig = async () => {
   const resp = await fetch(`${baseURL}/api/config`);
