d3441d01011ddd97d1cd1e735b5a546b3ba44d4e
angrylid <zm4399@gmail.com>
2022-08-14 21:08:45 +0800

    update url

--- lib/service/conf.ts
+++ lib/service/conf.ts
@@ -1,9 +1,10 @@
 import { FirebaseOptions, initializeApp } from "firebase/app";
 
 const baseURL =
-  process.env.NODE_ENV === "production"
-    ? location.origin
-    : "http://localhost:5669";
+  process.env.VERCEL_ENV === "development"
+    ? "http://localhost:5669"
+    : process.env.VERCEL_URL || "";
+
 const getConfig = async () => {
   const resp = await fetch(`${baseURL}/api/config`);
   const conf = (await resp.json()) as FirebaseOptions;
