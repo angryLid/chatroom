e2f3c95050bd17d687f5df03616c3649dbef5dac
husky <i@husky.doge>
2022-08-16 10:46:07 +0800

    add analyzer

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,19 @@
 import react from "@vitejs/plugin-react";
+import visualizer from "rollup-plugin-visualizer";
 import { defineConfig } from "vite";
+const plugins = [];
 
+if (process.env.NODE_ENV === "production") {
+  plugins.push(
+    visualizer({
+      open: true,
+      gzipSize: true,
+      brotliSize: true,
+    })
+  );
+}
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), ...plugins],
   server: {
     port: 5669,
   },
